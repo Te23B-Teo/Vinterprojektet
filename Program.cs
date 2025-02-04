905d409c8e03e5abbbcb019b972886ac6c6c9b1e
teo erfass josefsson <teo.erfassjosefsson@elev.ga.ntig.se>
2025-02-04 13:27:31 +0100

    noirfnjorgfv

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿Player p1 = new();
+Player p2 = new();
+
+p1.Name = "dnenfjenf";
+
+p2.Hitpoints -= 30;
+Console.WriteLine(p1.Hitpoints);
+
+
+
+
+Player p = new();
+
+Console.WriteLine(p.Name);
+Fight(p1
+Console.ReadLine();
+
+static void Fight(Player p){
+    Console.WriteLine(p.Name);
+}
+
+class Player
+{
+public string Name = "Micke";
+public int Hitpoints = 100;
+public int WeponDamage = 5;
+}
